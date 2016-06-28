64ddd0fec0a0edc33ceab53bbabd884236e3bf88
Marcos Santos <meliehl@outlook.com>
2016-06-28 19:06:30 -0300

    Domain and Api

---
+++ src/Api/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return "Hello!";
+        }
+    }
+}
