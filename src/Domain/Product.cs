7b9e5c15adec583040cfafbdd72b7f04fe9d66a7
Marcos Santos <meliehl@outlook.com>
2016-06-28 17:27:53 -0300

    Add project files.

---
+++ src/Domain/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Code { get; private set; }
+        public string Name { get; private set; }
+        
+        public Product(Guid Id,
+            string Code,
+            string Name)
+        {
+            this.Id = Id;
+            this.Code = Code;
+            this.Name = Name;
+        }
+    }
+}
