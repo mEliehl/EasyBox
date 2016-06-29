79aabb61d6a2b370783c5c353dcc37d345cf9850
Marcos Santos <meliehl@outlook.com>
2016-06-28 21:51:23 -0300

    Add some tests

--- src/Api/Controllers/ProductController.cs
+++ src/Api/Controllers/ProductController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
@@ -6,10 +10,18 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class ProductController
     {
-        [HttpGet]
-        public async Task<string> Get()
+        readonly IList<Product> products;
+
+        public ProductController()
         {
-            return "Hello!";
+            products = new List<Product>();
+            products.Add(new Product(Guid.NewGuid(), "usb-128", "Pendrive USB 128Gb"));
+        }
+
+        [HttpGet]
+        public async Task<Product> Get()
+        {
+            return products.FirstOrDefault();
         }
     }
 }
---
+++ test/Domain.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Domain.Test")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f45923a-84aa-46f1-998b-1c1a9d9ce98c")]
