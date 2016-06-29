f7c4b5752bb1687d47a1a8182e9054d7a09a79c6
Marcos Santos <meliehl@outlook.com>
2016-06-28 21:51:23 -0300

    Add some tests

---
+++ test/Domain.Test/Entities/ProductTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using Domain.Entities;
+
+namespace Domain.Test.Entities
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void ShouldCreateProduct()
+        {
+            var p = new Product(Guid.NewGuid(), "USB-128", "Pendrive usb 128gb");
+            Assert.NotNull(p);
+            Assert.NotEqual(p.Id, Guid.Empty);
+            Assert.NotEqual(p.Code, string.Empty);
+            Assert.NotEqual(p.Name, string.Empty);
+        }
+    }
+}
